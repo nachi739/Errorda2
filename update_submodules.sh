cc89902dfbaa78afa6533126863cb1c3dec9eef6
namiki <namiki081025@gmail.com>
2024-10-22 18:27:41 +0900

    Update submodules: chrome_extensions and errorda2_backend

---
+++ update_submodules.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# サブモジュールの更新
+git submodule update --remote
+
+# サブモジュールの変更をステージング
+git add chrome_extensions/
+git add errorda2_backend/
+
+# コミットメッセージ
+commit_message="Update submodules: chrome_extensions and errorda2_backend"
+
+# コミット
+git commit -m "$commit_message"
+
+# プッシュ
+git push origin main
