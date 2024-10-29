f0464b0125b8f0b027a2652ea1bc521abb1be5e6
namiki <namiki081025@gmail.com>
2024-10-29 19:03:16 +0900

    [update]commitメッセージ内容修正

--- update_submodules.sh
+++ update_submodules.sh
@@ -13,7 +13,7 @@ chrome_commit_message=$(cd chrome_extensions && git log -1 --pretty=%B)
 # chrome_extensionsに変更があるか確認
 if [ -n "$(git diff --cached --name-only chrome_extensions)" ]; then
     echo "chrome_extensionsのコミットメッセージを作成"
-    chrome_commit_message="[update] submodule: chrome_extensions: $chrome_commit_message"
+    chrome_commit_message="submodule:chrome_extensions:$chrome_commit_message"
 
     echo "chrome_extensionsのコミット"
     git commit -m "$chrome_commit_message"
@@ -34,7 +34,7 @@ errorda2_commit_message=$(cd errorda2_backend && git log -1 --pretty=%B)
 # errorda2_backendに変更があるか確認
 if [ -n "$(git diff --cached --name-only errorda2_backend)" ]; then
     echo "errorda2_backendのコミットメッセージを作成"
-    errorda2_commit_message="[update] submodule: errorda2_backend: $errorda2_commit_message"
+    errorda2_commit_message="submodule:errorda2_backend:$errorda2_commit_message"
 
     echo "errorda2_backendのコミット"
     git commit -m "$errorda2_commit_message"
