6002c5a55ba062d70960e11a3f83e513799f9aa7
namiki <namiki081025@gmail.com>
2024-11-12 14:32:38 +0900

    [update]関数・処理の修正

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,96 +1,97 @@
 #!/bin/bash
-
-# GitHub CLI (gh) がインストールされていることを確認してください
-# https://cli.github.com/
-# gh auth login でログインしてください
+# GitHub CLI (gh) がインストール・gh auth login が実行されていることを前提とする
+# 実行時ブランチはmainブランチであることを前提とする
 
 echo "サブモジュールの更新"
 git submodule update --remote
 
-echo "現在のブランチ名を取得"
+# 現在のブランチ名を取得
 current_branch=$(git branch --show-current)
 
-# サブモジュールのプルリクエストリンクを取得する関数
-get_submodule_pr_link() {
+# サブモジュールの最新のクローズされたプルリクエストリンクを取得する関数
+get_submodule_latest_closed_pr_link() {
     local repo=$1
-    local pr_link=$(gh pr list --repo "$repo" --state open --json url --jq '.[0].url')
+    local pr_link=$(gh pr list --repo "$repo" --state closed --sort updated --limit 1 --json url --jq '.[0].url')
+    if [ -z "$pr_link" ]; then
+        echo "No closed pull request found for $repo"
+    fi
     echo "$pr_link"
 }
 
-# chrome_extensionsの変更をステージングしてコミット
-echo "chrome_extensionsの変更をステージング"
-git add chrome_extensions/
 
-echo "chrome_extensionsの最新コミットメッセージを取得"
+# chrome_extensionsの最新コミットメッセージを取得
 chrome_commit_message=$(cd chrome_extensions && git log -1 --pretty=%B)
+# chromeのコミットメッセージ内容
+chrome_commit_message="submodule_chrome:$chrome_commit_message"
+# chromeの新しいブランチ名
+new_chrome_branch="dev-chrome_extensions-$(date +%Y%m%d%H)"
+# chromeのプルリクエストリンクを取得
+chrome_pr_link=$(get_submodule_latest_closed_pr_link "nachi739/errorda2_chrome_extensions")
+# chromeのPRの本文
+chrome_pr_body=$(echo -e "This PR updates the chrome_extensions submodule.\n\n## submodule-Pull-requests\n対象のサブモジュールのPull requestのリンク\n$chrome_pr_link")
+
+
+# --ここからchrome_extensionsの処理--
+echo "chrome_extensionsの変更をステージング(対象以外を含まないために)"
+git add chrome_extensions/
 
 # chrome_extensionsに変更があるか確認
 if [ -n "$(git diff --cached --name-only chrome_extensions)" ]; then
-    echo "chrome_extensionsのコミットメッセージを作成"
-    chrome_commit_message="submodule:chrome_extensions:$chrome_commit_message"
 
-    echo "新しいブランチを作成"
-    new_branch="dev-chrome_extensions-$(date +%Y%m%d%H)"
-    git checkout -b "$new_branch"
+    echo "新しいブランチを作成し切替"
+    git checkout -b "$new_chrome_branch"
 
-    echo "chrome_extensionsのコミット"
+    echo "chrome_extensionsのコミット・プッシュ"
     git commit -m "$chrome_commit_message"
-
-    echo "新しいブランチをプッシュ"
-    git push origin "$new_branch"
-
-    echo "サブモジュールのプルリクエストリンクを取得"
-    chrome_pr_link=$(get_submodule_pr_link "nachi739/errorda2_chrome_extensions")
+    git push origin "$new_chrome_branch"
 
     echo "プルリクエストを作成"
-    pr_body=$(echo -e "This PR updates the chrome_extensions submodule.\n\n## submodule-Pull-requests\n対象のサブモジュールのPull requestのリンク\n$chrome_pr_link")
-    gh pr create --title "$new_branch" --body "$pr_body" --base main --head "$new_branch"
-
+    gh pr create --title "$new_chrome_branch" --body "$chrome_pr_body" --base main --head "$new_chrome_branch"
 
     echo "元のブランチに戻る"
     git checkout "$current_branch"
 
-    echo "作製したブランチを削除（ローカルでは不要のため）"
-    git branch -D "$new_branch"
+    echo "作成したブランチを削除（ローカルでは不要のため）"
+    git branch -D "$new_chrome_branch"
 else
     echo "chrome_extensionsに変更はありません"
 fi
 
-# errorda2_backendの変更をステージングしてコミット
-echo "errorda2_backendの変更をステージング"
-git add errorda2_backend/
 
-echo "errorda2_backendの最新コミットメッセージを取得"
-errorda2_commit_message=$(cd errorda2_backend && git log -1 --pretty=%B)
+# errorda2_backendの最新コミットメッセージを取得
+backend_commit_message=$(cd errorda2_backend && git log -1 --pretty=%B)
+# backendのコミットメッセージ内容
+backend_commit_message="submodule_backend:$backend_commit_message"
+# backendの新しいブランチ名
+new_backend_branch="dev-errorda2_backend-$(date +%Y%m%d%H)"
+# backendのプルリクエストリンクを取得
+backend_pr_link=$(get_submodule_latest_closed_pr_link "nachi739/errorda2_backend")
+# backendのPRの本文
+backend_pr_body=$(echo -e "This PR updates the errorda2_backend submodule.\n\n## submodule-Pull-requests\n対象のサブモジュールのPull requestのリンク\n$backend_pr_link")
+
+
+# --ここからerrorda2_backendの処理--
+echo "errorda2_backendの変更をステージング(対象以外を含まないために)"
+git add errorda2_backend/
 
 # errorda2_backendに変更があるか確認
 if [ -n "$(git diff --cached --name-only errorda2_backend)" ]; then
-    echo "errorda2_backendのコミットメッセージを作成"
-    errorda2_commit_message="submodule:errorda2_backend:$errorda2_commit_message"
 
-    echo "新しいブランチを作成"
-    new_branch="dev-errorda2_backend-$(date +%Y%m%d%H)"
-    git checkout -b "$new_branch"
+    echo "新しいブランチを作成し切替"
+    git checkout -b "$new_backend_branch"
 
-    echo "errorda2_backendのコミット"
-    git commit -m "$errorda2_commit_message"
-
-    echo "新しいブランチをプッシュ"
-    git push origin "$new_branch"
-
-    echo "サブモジュールのプルリクエストリンクを取得"
-    backend_pr_link=$(get_submodule_pr_link "nachi739/errorda2_backend")
+    echo "errorda2_backendのコミット・プッシュ"
+    git commit -m "$backend_commit_message"
+    git push origin "$new_backend_branch"
 
     echo "プルリクエストを作成"
-    pr_body=$(echo -e "This PR updates the errorda2_backend submodule.\n\n## submodule-Pull-requests\n対象のサブモジュールのPull requestのリンク\n$backend_pr_link")
-    gh pr create --title "$new_branch" --body "$pr_body" --base main --head "$new_branch"
-
+    gh pr create --title "$new_backend_branch" --body "$backend_pr_body" --base main --head "$new_backend_branch"
 
     echo "元のブランチに戻る"
     git checkout "$current_branch"
 
-    echo "作製したブランチを削除（ローカルでは不要のため）"
-    git branch -D "$new_branch"
+    echo "作成したブランチを削除（ローカルでは不要のため）"
+    git branch -D "$new_backend_branch"
 else
     echo "errorda2_backendに変更はありません"
 fi
